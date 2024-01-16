c4747f345e40f0f505cb47c69d2b7c710a7d38ab
Nikita Demyanenko <nikitademyanenko000@gmail.com]>
2024-01-16 20:34:32 +0300

    feat: add storybook, add 3 storybook decorators, add storybook tests for all ui components

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -5,13 +5,13 @@ import { Button, ThemeButton } from "./Button";
 describe("Button", () => {
     test("Test render", () => {
         render(<Button>TEST</Button>);
-        expect(screen.getByText(testButtonText))
+        expect(screen.getByText("TEST"))
             .toBeInTheDocument();
     });
 
     test("Test clear theme", () => {
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
-        expect(screen.getByText(testButtonText))
+        expect(screen.getByText("TEST"))
             .toHaveClass("clear");
         screen.debug();
     });
