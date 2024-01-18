a727909cf5e16ae38b819619d1d52c4de8368996
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2024-01-18 16:54:48 +0300

    feat: add main pipeline github actions

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,13 @@
+import { FC } from "react";
 import { useTranslation } from "react-i18next";
 
-function MainPage() {
+const MainPage:FC = () => {
     const { t } = useTranslation("main");
     return (
         <div>
             {t("Главная страница")}
         </div>
     );
-}
+};
 
 export default MainPage;
