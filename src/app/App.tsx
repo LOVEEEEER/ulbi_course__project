9e4e887e84c4a04e6fbef2e087861b93bdc41212
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2024-01-28 18:36:21 +0300

    refactor: add global theme className on body tag

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,12 @@
 import { Suspense } from "react";
-import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { AppRouter } from "./providers/router";
 
 function App() {
-    const { theme } = useTheme();
-
     return (
-        <div className={classNames("app", {}, [theme])}>
+        <div className={classNames("app", {}, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
