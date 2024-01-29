ef59bb1bd780136c2a3bbaaaef0e781dadad5bb5
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2024-01-29 08:18:49 +0300

    refactor: fix stylelint errors

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,15 +1,13 @@
 import { FC } from "react";
-import { classNames } from "shared/lib/classNames/classNames";
 import { Modal } from "shared/ui/Modal/Modal";
 import { LoginForm } from "../LoginForm/LoginForm";
 
 interface LoginModalProps {
-    className?: string,
     isOpen: boolean,
     onClose: () => void
 }
 
-export const LoginModal: FC<LoginModalProps> = ({ className, isOpen, onClose }) => (
+export const LoginModal: FC<LoginModalProps> = ({ isOpen, onClose }) => (
     <Modal isOpen={isOpen} onClose={onClose} lazy>
         <LoginForm />
     </Modal>
